a7fd2b07a961e174ef9488f6d88fe91813700108
Kaushik Karthikeyan <Kaushik.karthik02@gmail.com>
2024-09-26 14:46:16 -0400

    Create NavBar.ts

---
+++ src/components/NavBar.ts
@@ -0,0 +1,25 @@
+// src/components/NavBar.tsx
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+
+const NavBar: React.FC = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Hist Viewer
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
+
